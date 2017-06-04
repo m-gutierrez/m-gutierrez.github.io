65d5690e44cb44a0451a39e82bbc6e5f38974a07
Michael Gutierrez <M_G@mit.edu>
2017-06-03 19:59:21 -0700

    updated favorite images

--- _posts/2017-06-02-Favorite-photos.md
+++ _posts/2017-06-02-Favorite-photos.md
@@ -5,15 +5,11 @@ image: /images/2016-10-05/HOA_OVEN_THUMB.jpg
 date: "2017-06-02"
 ---
 
-This is a testing, 123 how about now? can you here me? 
-
 <!-- Gallery __-->			
-{% include subgallery-add.html gal="gal1_favphotos" img=1%}
-
-{% include subgallery-add.html gal="gal1_favphotos" img=0%}
-
-{% include subgallery-add.html gal="gal1_favphotos" img=1%}
-
-{% include subgallery-add.html gal="gal1_favphotos" img=0%}
+{% include subgallery.html gal="gal1_favphotos" %}
 <!-- end of GALLERY __ -->
+
+
+These are some of my favorite photos that I've taken. 
+
 
--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
 			
 			if ( !$fullscreen_gallery.hasClass('kenburns-gallery') ) {	// do not initialize if kenburns
 				jQuery('#footer').prepend('<nav id="gallerynav"><a href="#" class="thumbs">	<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">	<rect width="5" height="5" x="0"   y="0" /><rect width="5" height="5" x="8" y="0" /><rect width="5" height="5" x="0" y="8" /><rect width="5" height="5" x="8" y="8" /></svg></a><a href="#" class="prev">&lt;</a> <a href="#" class="pause">&#9614;&#9614;</a> <a href="#" class="next">&gt;</a></nav>');
-
+				console.log(1 + 6);
 				$fullscreen_gallery.before('<ul id="gallerythumbs">').cycle({
 					slideExpr: '.gallery-item',
 					fx:        'fade', 
