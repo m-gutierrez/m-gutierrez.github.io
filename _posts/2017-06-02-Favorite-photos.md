11621a3b74a204bbc7c29a49e6b049188324dc89
Michael Gutierrez <M_G@mit.edu>
2017-06-04 16:59:14 -0700

    updated syntax highlighting

--- _posts/2017-06-02-Favorite-photos.md
+++ _posts/2017-06-02-Favorite-photos.md
@@ -10,4 +10,9 @@ date: "2017-06-02"
 <!-- end of GALLERY __ -->
 
 
-These are some of my favorite photos that I've taken. Mostly doing this to try and get image handling slightly nicer than just directly embedding the full res jpgs. Solution so far is a mix of jquery [magnific popup](http://dimsemenov.com/plugins/magnific-popup/) and [photorama's](https://github.com/sunbliss/photorama) galleries. I've changed the "<img src" to use a liquid filter serving half-sized images within the post, magnific takes the href
+These are some of my favorite photos that I've taken. Mostly doing this to try and get image handling slightly nicer than just directly embedding the full res jpgs. Solution so far is a mix of jquery [magnific popup](http://dimsemenov.com/plugins/magnific-popup/) and [photorama's](https://github.com/sunbliss/photorama) galleries. I've changed the "<img src" to use a liquid filter serving half-sized images within the post, magnific takes the href value for displaying the full screen image. To this end I also wrote a script for processing an gallery image files. Using [imagemagick](http://www.imagemagick.org/script/index.php) the script resizes all images to 1600 x ? for full resolution displayed, it copies and reduces these images by 30% for displaying in-line and lastly it runs [imageOptim](https://imageoptim.com/) on all images to further minimize the filesize. 
+
+
+{% highlight python %}
+{% include image_resize.py %}
+{% endhighlight %}
--- image_resize.py
+++ image_resize.py
@@ -8,47 +8,53 @@ cwd = os.getcwd()
 
 debug = False
 
-parser = argparse.ArgumentParser(description="Universal server black-boxing the communication with individual equipment")
+parser = argparse.ArgumentParser(description="Image optimization for blog")
 
 parser.add_argument("-d", "--directory", default=".", type=str, help="specify working directory")
-parser.add_argument("-o", "--operation", default="convert", type=str, help=" resize convert copyAndResize optimize")
+parser.add_argument("-o", "--operation", default="complete", type=str, help="complete resize convert copyAndResize optimize")
 
 
-args = vars(parser.parse_args(sys.argv[1:]))
-#args["directory"] = cwd + args["directory"]
+in_args = vars(parser.parse_args(sys.argv[1:]))
 
-print args["directory"]
+if in_args["operation"] == "complete":
+	in_args["operation"] = ["convert", "resize", "copyAndResize", "optimize"]
+
+print "Operating directory ", args["directory"]
 
 
+for args in in_args["operation"]:
 
-
-if args["operation"] == "optimize":
-	cmdoptim ="open -a ImageOptim %s/"%(args["directory"]) 
-	cmdoptim =cmdoptim.split(' ')
-	print cmdoptim
-	call( cmdoptim)
-elif args["operation"] == "resize":
-	cmdr = 'mogrify -resize 1600> %s/*.jpg'%(args["directory"])
-	cmdr = cmdr.split(' ')
-	print cmdr
-	call( cmdr)
-	cmdr = 'mogrify -resize 1600> %s/*.png'%(args["directory"])
-	cmdr = cmdr.split(' ')
-	print cmdr
-	call( cmdr)
-elif args["operation"] == "copyAndResize":
-	cmd2x = 'convert %s/*.jpg -resize 30%% -set filename:area %%t_post %s/%%[filename:area].jpg'%(args["directory"],args["directory"])
-	cmd2x = cmd2x.split(' ')
-	print cmd2x
-	call( cmd2x)
-	cmd2x = 'convert %s/*.png -resize 30%% -set filename:area %%t_post %s/%%[filename:area].png'%(args["directory"],args["directory"])
-	cmd2x = cmd2x.split(' ')
-	print cmd2x
-	call( cmd2x)
-elif args["operation"] == "convert":
-	cmdconvert ="mogrify -format jpg %s/*.JPG"%args["directory"]
-	cmdconvert = cmdconvert.split(' ')
-	print cmdconvert
-	call( cmdconvert)
-else:
-	print 'available cmds: resize convert copyAndResize optimize '
+	if args["operation"] == "optimize":
+		cmdoptim ="open -a ImageOptim %s/"%(args["directory"]) 
+		cmdoptim =cmdoptim.split(' ')
+		print cmdoptim
+		call( cmdoptim)
+	elif args["operation"] == "resize":
+		cmdr = 'mogrify -resize 1600> %s/*.jpg'%(args["directory"])
+		cmdr = cmdr.split(' ')
+		print cmdr
+		call( cmdr)
+		cmdr = 'mogrify -resize 1600> %s/*.png'%(args["directory"])
+		cmdr = cmdr.split(' ')
+		print cmdr
+		call( cmdr)
+	elif args["operation"] == "copyAndResize":
+		cmd2x = 'convert %s/*.jpg -resize 30%% -set filename:area %%t_post %s/%%[filename:area].jpg'%(args["directory"],args["directory"])
+		cmd2x = cmd2x.split(' ')
+		print cmd2x
+		call( cmd2x)
+		cmd2x = 'convert %s/*.png -resize 30%% -set filename:area %%t_post %s/%%[filename:area].png'%(args["directory"],args["directory"])
+		cmd2x = cmd2x.split(' ')
+		print cmd2x
+		call( cmd2x)
+	elif args["operation"] == "convert":
+		cmdconvert ="mogrify -format jpg %s/*.JPG"%args["directory"]
+		cmdconvert = cmdconvert.split(' ')
+		print cmdconvert
+		call( cmdconvert)
+		cmdconvert ="mogrify -format png %s/*.PNG"%args["directory"]
+		cmdconvert = cmdconvert.split(' ')
+		print cmdconvert
+		call( cmdconvert)
+	else:
+		print 'available cmds: resize convert copyAndResize optimize '
